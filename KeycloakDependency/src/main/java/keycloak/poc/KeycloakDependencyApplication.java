ec714ab95da963a7c9e93a690dee1489c62b0fbb
Pavan BTD <pavanpwm@gmail.com>
2021-10-24 15:04:33 +0530

    Add files via upload

---
+++ KeycloakDependency/src/main/java/keycloak/poc/KeycloakDependencyApplication.java
@@ -0,0 +1,13 @@
+package keycloak.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakDependencyApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakDependencyApplication.class, args);
+	}
+
+}
